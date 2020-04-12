75f9b1895e433eaa3e7cf4141c3abb0863fd8fb6
Jonathan W. Cranford <jonathan.w.cranford@gmail.com>
2020-04-12 17:14:42 -0400

    1.2 - now repeats until guess the right answer

--- guess.lua
+++ guess.lua
@@ -1,20 +1,21 @@
 -- guess.lua
 
 print("Guess a number between 1 and 20")
-guess = io.read("*number") 
 number = 13
-print("You guessed", guess)
 
-if guess == number then
-  print("You guessed right!")
-elseif guess < 1 then
-  print("You don't follow directions very well, do you?")
-elseif guess > 20 then
-  print("Do you know how to play this game?")
-elseif guess < number then
-  print("You guessed too low!")
-else
-  print("You guessed too high!")
-end
-	
+repeat
+	guess = io.read("*number") 
+	print("You guessed", guess)
 
+	if guess == number then
+	  print("You guessed right!")
+	elseif guess < 1 then
+	  print("You don't follow directions very well, do you?")
+	elseif guess > 20 then
+	  print("Do you know how to play this game?")
+	elseif guess < number then
+	  print("You guessed too low!")
+	else
+	  print("You guessed too high!")
+	end
+until guess == number
